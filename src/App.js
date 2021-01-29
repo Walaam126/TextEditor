ba5871ae19e866fe059f5a4e8e77b0a276a64ff1
Walaam126 <walaamajeed12@gmail.com>
2021-01-30 01:21:44 +0300

    texteditorV2

--- src/App.js
+++ src/App.js
@@ -11,28 +11,37 @@ const stylings = ["bold", "italic", "underline"];
 const colors = ["yellow", "blue", "red", "black", "purple"];
 
 function App() {
- 
-  const [textarea, setTextarea] = useState({color: "",fontStyle:"",fontWeight:"",textDecorationLine:""});
+  
+  const [textarea, setTextarea] = useState({ color: "", fontStyle: "", fontWeight: "", textDecorationLine: "" });
+
   //set color
-  function setfont(e) {
-    let setcolor=e.target.key;
-    console.log(setcolor);
-    setTextarea(prevtextarea => ({
-      ...prevtextarea,color:key
-    }));
+ const settxtColor=(color) => {
+ setTextarea(prevtextarea => ({
+      ...prevtextarea,color:color
+ }));
+ }
+  
+  //set font style
+  const settxtFont = (Font, e) => {
+    const fprop = Object.keys(Font)[0];
+    if (textarea[fprop] === "") {
+      setTextarea(prevtextarea => ({
+        ...prevtextarea, ...Font
+      }));
+      e.target.className = "btn btn-primary";
+    } else {
+      const newobj = {};
+      newobj[fprop] = "";
+      setTextarea(prevtextarea => ({
+        ...prevtextarea, ...newobj
+      }));
+      e.target.className = "btn btn-light";
+    }
   }
-
-  //font style set
- function setfont(e){
-   let fstyle=e.target.getAttribute('style'); 
-    setTextarea(prevtextarea => ({
-      ...prevtextarea,fstyle
-    }));
-   } 
- 
-
+  
+  //set button style
   const stylingBoxes = stylings.map((style) => (
-    <button className="btn btn-light" style={styles[style]} key={style}>
+    <button className="btn btn-light" style={styles[style]} key={style} onClick={(e) => settxtFont(styles[style],e)}>
       {style}
     </button>
   ));
@@ -40,15 +49,15 @@ function App() {
   const colorBoxes = colors.map((color) => (
     <button
       style={{ backgroundColor: color, height: 30, width: 30 }}
-      key={color}
+      key={color} onClick={() => settxtColor(color)}
     />
   ));
 
   return (
     <div className="App">
-      <div className="my-3" onClick={(e) => setfont(e)}>{stylingBoxes}</div>
+      <div className="my-3">{stylingBoxes}</div>
       <textarea style={textarea} />
-      <div className="my-3" onClick={(e) => changeColor(e)}>{colorBoxes}</div>
+      <div className="my-3">{colorBoxes}</div>
     </div>
   );
 }
